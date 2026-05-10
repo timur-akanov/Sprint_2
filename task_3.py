78d93707685e3b36aa6be7db672e79c81d3333c4
timur-akanov <timur-akanov@mail.ru>
2026-05-10 10:52:53 +0500

    task3

--- task_3.py
+++ task_3.py
@@ -1,36 +1,26 @@
 class PointsForPlace:
-    def __init__(self):
-        self.points = 0
-
-    def get_points_for_place(self, place: int):
+    @staticmethod
+    def get_points_for_place(place: int):
         if place > 100:
             print('Баллы начисляются только первым 100 участникам')
-            self.points = 0
+            return 0
         elif place < 1:
             print('Спортсмен не может занять нулевое или отрицательное место')
-            self.points = 0
+            return 0
         else:
-            self.points = 101 - int(place)
-        return self.points
-
+            return 101 - int(place)
 
 class PointsForMeters:
-    def __init__(self):
-        self.points = 0
-
-    def get_points_for_meters(self, meters: int):
+    @staticmethod
+    def get_points_for_meters(meters: int):
         if meters < 0:
             print('Количество метров не может быть отрицательным')
-            self.points = 0
-        else:
-            self.points = meters * 0.5
-        return self.points
+            return 0
+        return meters * 0.5
 
 
 class TotalPoints(PointsForPlace, PointsForMeters):
     def __init__(self):
-        PointsForPlace.__init__(self)
-        PointsForMeters.__init__(self)
         self.total = 0
 
     def get_total_points(self, meters: int, place: int):
@@ -40,15 +30,10 @@ class TotalPoints(PointsForPlace, PointsForMeters):
         return self.total
 
 
+print(PointsForPlace.get_points_for_place(10))   
+print(PointsForMeters.get_points_for_meters(10)) 
 
-
-points_for_place = PointsForPlace()
-print(points_for_place.get_points_for_place(10))
-
-points_for_meters = PointsForMeters()
-print(points_for_meters.get_points_for_meters(10))
-
-total_points = TotalPoints()
-print(total_points.get_points_for_place(10))
-print(total_points.get_points_for_meters(10))
-print(total_points.get_total_points(100, 10)) 
+tp = TotalPoints()
+print(tp.get_points_for_place(10))               
+print(tp.get_points_for_meters(10))              
+print(tp.get_total_points(10, 10))               
